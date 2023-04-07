0c507ed9c6867a0aae71e90897397c67bf2950e9
suhjaesuk <suhjaesuk@gmail.com>
2023-04-07 21:28:00 +0900

    feat: security 관련 Member 도메인 클래스 추가 및 기능 개발

---
+++ src/main/java/com/numble/webnovelservice/member/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.numble.webnovelservice.member.repository;
+
+import com.numble.webnovelservice.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+}
+
