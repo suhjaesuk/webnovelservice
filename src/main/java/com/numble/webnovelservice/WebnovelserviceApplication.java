44fc33f10ae826508e55dc59043001469754e59a
suhjaesuk <suhjaesuk@gmail.com>
2023-04-07 09:42:54 +0900

    git init

---
+++ src/main/java/com/numble/webnovelservice/WebnovelserviceApplication.java
@@ -0,0 +1,13 @@
+package com.numble.webnovelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebnovelserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebnovelserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/numble/webnovelservice/WebnovelserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numble.webnovelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebnovelserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
