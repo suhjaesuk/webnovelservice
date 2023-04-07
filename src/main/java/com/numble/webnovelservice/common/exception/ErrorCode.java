da5644e8661559c2ff3265be72ac7f0b38a18c6c
suhjaesuk <suhjaesuk@gmail.com>
2023-04-07 21:24:20 +0900

    feat: security 관련 ErrorCode 추가

--- src/main/java/com/numble/webnovelservice/common/exception/ErrorCode.java
+++ src/main/java/com/numble/webnovelservice/common/exception/ErrorCode.java
@@ -13,6 +13,14 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    INVALID_ACCESS_TOKEN(HttpStatus.BAD_REQUEST, "JWT_001", "Access Token이 유효하지 않습니다."),
+    EXPIRATION_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "JWT_002", "Access Token이 만료되었습니다"),
+    ACCESS_TOKEN_NOT_SUPPORT(HttpStatus.UNAUTHORIZED, "JWT_003", "지원하지 않는 Access Token입니다"),
+    UNKNOWN_ACCESS_TOKEN_ERROR(HttpStatus.UNAUTHORIZED, "JWT_004", "Access Token 에러입니다"),
+    UNKNOWN_TOKEN_ERROR(HttpStatus.BAD_REQUEST, "JWT_005", "알 수 없는 토큰 에러입니다"),
+
+    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "MEMBER_001", "찾을 수 없는 회원입니다."),
+    NOT_AUTHORIZED_MEMBER(HttpStatus.BAD_REQUEST, "MEMBER_002", "인가되지 않은 사용자입니다."),
     ;
 
     private final HttpStatus httpStatus;
