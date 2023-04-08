5c7e9f04d39b55222d2a7f97228452b0b65bbce8
suhjaesuk <suhjaesuk@gmail.com>
2023-04-08 12:47:34 +0900

    feat: 로그인 기능 개발

--- src/main/java/com/numble/webnovelservice/member/service/MemberService.java
+++ src/main/java/com/numble/webnovelservice/member/service/MemberService.java
@@ -1,9 +1,11 @@
 package com.numble.webnovelservice.member.service;
 
+import com.numble.webnovelservice.common.exception.WebNovelServiceException;
 import com.numble.webnovelservice.member.dto.request.MemberLoginRequest;
 import com.numble.webnovelservice.member.dto.request.MemberSignUpRequest;
 import com.numble.webnovelservice.member.entity.Member;
 import com.numble.webnovelservice.member.repository.MemberRepository;
+import com.numble.webnovelservice.util.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,28 +13,50 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletResponse;
 
+import static com.numble.webnovelservice.common.exception.ErrorCode.NOT_FOUND_MEMBER;
+import static com.numble.webnovelservice.common.exception.ErrorCode.NOT_VALID_PASSWORD;
+import static com.numble.webnovelservice.util.jwt.JwtUtil.AUTHORIZATION_ACCESS;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     @Transactional
     public void signUp(MemberSignUpRequest request) {
 
-        String password = passwordEncoder.encode(request.getPassword());
-        Member member = request.toMember(password);
+        String encryptedPassword = passwordEncoder.encode(request.getPassword());
+        Member member = request.toMember(encryptedPassword);
+
         memberRepository.save(member);
     }
 
     @Transactional
     public void login(MemberLoginRequest request, HttpServletResponse response) {
 
-        /*
-        1. 요청이 오면 유저아이디(username)으로 찾는다.
-        2. 해당 아이디의 비밀번호와 일치하는지 판별
-        3. 토큰 발행
-        */
+        Member member = memberRepository.findByUsername(request.getUsername()).orElseThrow(
+                () -> new WebNovelServiceException(NOT_FOUND_MEMBER));
+
+        validatePasswordMatch(request.getPassword(), member.getPassword());
+
+        issueTokens(response, member.getUsername());
+    }
+
+    @Transactional
+    public void issueTokens(HttpServletResponse response, String email){
+
+        String accessToken = jwtUtil.createAccessToken(email);
+        response.addHeader(AUTHORIZATION_ACCESS, accessToken);
+    }
+
+    public void validatePasswordMatch(String encryptedPassword, String inputPassword) {
+
+        if (passwordEncoder.matches(inputPassword, encryptedPassword)) {
+            return;
+        }
+        throw new WebNovelServiceException(NOT_VALID_PASSWORD);
     }
 }
