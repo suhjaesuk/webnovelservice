904a4a3f985574c1f2f5099f9702976d26e2e933
suhjaesuk <suhjaesuk@gmail.com>
2023-04-08 12:49:24 +0900

    form: 매개변수명 password를 encryptedPassword로 구체적으로 변경

--- src/main/java/com/numble/webnovelservice/member/dto/request/MemberSignUpRequest.java
+++ src/main/java/com/numble/webnovelservice/member/dto/request/MemberSignUpRequest.java
@@ -11,13 +11,13 @@ public class MemberSignUpRequest {
     private String password;
 
     /*
-    password 변수는 암호화 후 매개변수를 통해 DB에 저장한다.
+    password 변수는 MemberService에서 암호화 후 매개변수를 통해 DB에 저장한다.
      */
-    public Member toMember(String password){
+    public Member toMember(String encryptedPassword){
         return Member.builder()
                 .username(username)
                 .nickname(nickname)
-                .password(password)
+                .password(encryptedPassword)
                 .profileImage("profileImage")
                 .pointAmount(0)
                 .ticketCount(0)
