6b254d0595a3d392954ad027c70e900357b4821f
suhjaesuk <suhjaesuk@gmail.com>
2023-04-08 14:32:30 +0900

    fix: Controller에 매개변수에 @RequestBody를 누락하여 생긴 오류 해결

--- src/main/java/com/numble/webnovelservice/member/controller/MemberController.java
+++ src/main/java/com/numble/webnovelservice/member/controller/MemberController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,15 +22,14 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/auth/sign-up")
-    public ResponseEntity<ResponseMessage<Void>> signUp(MemberSignUpRequest request){
+    public ResponseEntity<ResponseMessage<Void>> signUp(@RequestBody MemberSignUpRequest request){
         memberService.signUp(request);
         return new ResponseEntity<>(new ResponseMessage<>("회원가입 성공",null), HttpStatus.CREATED);
     }
 
     @PostMapping("/auth/login")
-    public ResponseEntity<ResponseMessage<Void>> login(MemberLoginRequest request, HttpServletResponse response){
+    public ResponseEntity<ResponseMessage<Void>> login(@RequestBody MemberLoginRequest request, HttpServletResponse response){
         memberService.login(request, response);
         return new ResponseEntity<>(new ResponseMessage<>("로그인 성공",null), HttpStatus.OK);
     }
-
 }
