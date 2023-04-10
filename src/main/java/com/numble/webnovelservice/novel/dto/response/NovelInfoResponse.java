79082c7cdc18e677d5a64148f786ccce595211d0
suhjaesuk <suhjaesuk@gmail.com>
2023-04-10 16:20:31 +0900

    feat: 소설 정보 조회 시 필요한 response dto 클래스 추가 및 코드 작성

---
+++ src/main/java/com/numble/webnovelservice/novel/dto/response/NovelInfoResponse.java
@@ -0,0 +1,67 @@
+package com.numble.webnovelservice.novel.dto.response;
+
+import com.numble.webnovelservice.novel.entity.Genre;
+import com.numble.webnovelservice.novel.entity.Novel;
+import com.numble.webnovelservice.novel.entity.SerializedStatus;
+import com.numble.webnovelservice.util.time.TimeConverter;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Optional;
+
+@Getter
+public class NovelInfoResponse {
+
+    private Long novelId;
+
+    private String title;
+
+    private String author;
+
+    private String genre;
+
+    private String coverImage;
+
+    private String serializedStatus;
+
+    private Integer likeCount;
+
+    private Integer totalViewCount;
+
+    private String updatedAt;
+
+    @Builder
+    public NovelInfoResponse(Long novelId, String title, String author, String genre, String coverImage, String serializedStatus, Integer likeCount, Integer totalViewCount, String updatedAt) {
+
+        this.novelId = novelId;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.coverImage = coverImage;
+        this.serializedStatus = serializedStatus;
+        this.likeCount = likeCount;
+        this.totalViewCount = totalViewCount;
+        this.updatedAt = updatedAt;
+    }
+
+    public static NovelInfoResponse toResponse(Novel novel){
+
+        String koreanGenre = Genre.toKoreanName(novel.getGenre());
+        String koreanSerializedStatus = SerializedStatus.toKoreanName(novel.getSerializedStatus());
+        String convertedUpdatedAt = Optional.ofNullable(novel.getUpdatedAt())
+                                            .map(TimeConverter::toStringFormat)
+                                            .orElse(null);
+
+        return NovelInfoResponse.builder()
+                .novelId(novel.getId())
+                .title(novel.getTitle())
+                .author(novel.getAuthor())
+                .genre(koreanGenre)
+                .coverImage(novel.getCoverImage())
+                .serializedStatus(koreanSerializedStatus)
+                .likeCount(novel.getLikeCount())
+                .totalViewCount(novel.getTotalViewCount())
+                .updatedAt(convertedUpdatedAt)
+                .build();
+    }
+}
