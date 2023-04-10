f1aac875fe284c2703e8231b7b6d21be10242107
suhjaesuk <suhjaesuk@gmail.com>
2023-04-10 13:21:45 +0900

    feat: Entity Enum 필드 및 한국어를 Enum으로 변환하는 메서드 추가

---
+++ src/main/java/com/numble/webnovelservice/novel/entity/SerializedStatus.java
@@ -0,0 +1,29 @@
+package com.numble.webnovelservice.novel.entity;
+
+import com.numble.webnovelservice.common.exception.WebNovelServiceException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static com.numble.webnovelservice.common.exception.ErrorCode.NOT_VALID_SERIALIZED_STATUS;
+
+@Getter
+@AllArgsConstructor
+public enum SerializedStatus {
+
+    SERIALIZING("연재 중"),
+    PAUSED("휴재"),
+    FINISHED("연재 완료"),
+    ;
+
+    private final String koreanName;
+
+    public static SerializedStatus fromKoreanName(String serializedStatus) {
+
+        return switch (serializedStatus) {
+            case "연재 중" -> SERIALIZING;
+            case "휴재" -> PAUSED;
+            case "연재 완료" -> FINISHED;
+            default -> throw new WebNovelServiceException(NOT_VALID_SERIALIZED_STATUS);
+        };
+    }
+}
