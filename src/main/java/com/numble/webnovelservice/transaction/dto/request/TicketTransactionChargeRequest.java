569368e0c96369e8e98b8218084fe4b6a6e31ca7
suhjaesuk <suhjaesuk@gmail.com>
2023-04-12 15:37:08 +0900

    feat: 티켓 충전 시 필요한 request dto 클래스 추가 및 코드 작성

---
+++ src/main/java/com/numble/webnovelservice/transaction/dto/request/TicketTransactionChargeRequest.java
@@ -0,0 +1,42 @@
+package com.numble.webnovelservice.transaction.dto.request;
+
+import com.numble.webnovelservice.member.entity.Member;
+import com.numble.webnovelservice.transaction.entity.PointTransaction;
+import com.numble.webnovelservice.transaction.entity.TicketTransaction;
+import com.numble.webnovelservice.transaction.entity.Type;
+import lombok.Getter;
+
+import static com.numble.webnovelservice.transaction.entity.Type.CHARGE;
+import static com.numble.webnovelservice.transaction.entity.Type.CONSUME;
+
+@Getter
+public class TicketTransactionChargeRequest {
+
+    private Integer amount;
+
+    public TicketTransaction toTicketTransaction(Member member){
+
+        Type type = CHARGE;
+        Integer balance = member.getTicketCount() + amount;
+
+        return TicketTransaction.builder()
+                .amount(amount)
+                .balance(balance)
+                .type(type)
+                .member(member)
+                .build();
+    }
+
+    public PointTransaction toPointTransaction(Member member){
+
+        Type type = CONSUME;
+        Integer balance = member.getPointAmount() - amount;
+
+        return PointTransaction.builder()
+                .amount(amount)
+                .balance(balance)
+                .type(type)
+                .member(member)
+                .build();
+    }
+}
