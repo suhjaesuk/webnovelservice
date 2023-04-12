9fee73c3abb2a0dcc6ff075efc39ce1eb3e086dc
suhjaesuk <suhjaesuk@gmail.com>
2023-04-12 10:41:02 +0900

    feat: Transcation을 상속하는 PointTranscation 엔티티 추가

---
+++ src/main/java/com/numble/webnovelservice/transaction/entity/PointTransaction.java
@@ -0,0 +1,30 @@
+package com.numble.webnovelservice.transaction.entity;
+
+import com.numble.webnovelservice.member.entity.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PointTransaction extends Transaction{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="point_transaction_id")
+    private Long id;
+
+    @Builder
+    public PointTransaction(Type type, Integer amount, Integer balance, Member member, Long id) {
+        super(type, amount, balance, member);
+        this.id = id;
+    }
+}
