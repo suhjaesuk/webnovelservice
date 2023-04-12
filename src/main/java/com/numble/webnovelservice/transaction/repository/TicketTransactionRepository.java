17730888b00f2e20fc9a1008fdf84265c9952362
suhjaesuk <suhjaesuk@gmail.com>
2023-04-12 15:39:19 +0900

    feat: TicketTransaction 레포지토리 인터페이스 추가 및 코드 작성

---
+++ src/main/java/com/numble/webnovelservice/transaction/repository/TicketTransactionRepository.java
@@ -0,0 +1,12 @@
+package com.numble.webnovelservice.transaction.repository;
+
+import com.numble.webnovelservice.member.entity.Member;
+import com.numble.webnovelservice.transaction.entity.TicketTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TicketTransactionRepository extends JpaRepository<TicketTransaction, Long> {
+
+    List<TicketTransaction> findByMember(Member member);
+}
