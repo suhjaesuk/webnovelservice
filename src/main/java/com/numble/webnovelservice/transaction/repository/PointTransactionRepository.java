ddba21cd72729ff13a81e66f99c730b6587b9473
suhjaesuk <suhjaesuk@gmail.com>
2023-04-12 10:41:30 +0900

    feat: PointTranscationRepository 인터페이스 추가 및 회원 객체로 찾는 메서드 추가

---
+++ src/main/java/com/numble/webnovelservice/transaction/repository/PointTransactionRepository.java
@@ -0,0 +1,12 @@
+package com.numble.webnovelservice.transaction.repository;
+
+import com.numble.webnovelservice.member.entity.Member;
+import com.numble.webnovelservice.transaction.entity.PointTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PointTransactionRepository extends JpaRepository<PointTransaction, Long> {
+
+    List<PointTransaction> findByMember(Member member);
+}
