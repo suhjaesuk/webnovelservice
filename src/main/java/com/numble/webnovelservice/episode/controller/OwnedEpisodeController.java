536c18228b3e30714e8dcf0adb79695d30542c99
suhjaesuk <suhjaesuk@gmail.com>
2023-04-13 14:05:22 +0900

    feat: 소장 에피소드 조회 코드 작성

--- src/main/java/com/numble/webnovelservice/episode/controller/OwnedEpisodeController.java
+++ src/main/java/com/numble/webnovelservice/episode/controller/OwnedEpisodeController.java
@@ -1,6 +1,7 @@
 package com.numble.webnovelservice.episode.controller;
 
 import com.numble.webnovelservice.common.response.ResponseMessage;
+import com.numble.webnovelservice.episode.dto.response.OwnedEpisodeInfoResponseList;
 import com.numble.webnovelservice.episode.dto.response.OwnedEpisodeReadResponse;
 import com.numble.webnovelservice.episode.service.OwnedEpisodeService;
 import com.numble.webnovelservice.util.security.UserDetailsImpl;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,12 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/episodes/{episodeId}")
+@RequestMapping("/api/episodes")
 public class OwnedEpisodeController {
 
     private final OwnedEpisodeService ownedEpisodeService;
 
-    @PostMapping
+    @PostMapping("/{episodeId}")
     public ResponseEntity<ResponseMessage<Void>> purchaseEpisode(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                                  @PathVariable Long episodeId){
 
@@ -29,7 +31,7 @@ public class OwnedEpisodeController {
         return new ResponseEntity<>(new ResponseMessage<>("에피소드 구매 성공",null), HttpStatus.CREATED);
     }
 
-    @PutMapping
+    @PutMapping("/{episodeId}")
     public ResponseEntity<ResponseMessage<OwnedEpisodeReadResponse>> readOwnedEpisode(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                                                       @PathVariable Long episodeId){
 
@@ -37,7 +39,7 @@ public class OwnedEpisodeController {
         return new ResponseEntity<>(new ResponseMessage<>("에피소드 열람 성공",response), HttpStatus.OK);
     }
 
-    @PutMapping
+    @PutMapping("/{episodeId}")
     public ResponseEntity<ResponseMessage<Void>> readOwnedEpisodeNextPage(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                                             @PathVariable Long episodeId){
 
@@ -45,11 +47,18 @@ public class OwnedEpisodeController {
         return new ResponseEntity<>(new ResponseMessage<>("에피소드 다음 페이지 읽기 성공", null), HttpStatus.OK);
     }
 
-    @PutMapping
+    @PutMapping("/{episodeId}")
     public ResponseEntity<ResponseMessage<Void>> readOwnedEpisodePreviousPage(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                                           @PathVariable Long episodeId){
 
         ownedEpisodeService.readOwnedEpisodePreviousPage(userDetails.getMember(), episodeId);
         return new ResponseEntity<>(new ResponseMessage<>("에피소드 이전 페이지 읽기 성공", null), HttpStatus.OK);
+    }
+
+    @GetMapping("/owned-episode")
+    public ResponseEntity<ResponseMessage<OwnedEpisodeInfoResponseList>> retrieveOwnedEpisodesByMember(@AuthenticationPrincipal UserDetailsImpl userDetails){
+
+        OwnedEpisodeInfoResponseList response = ownedEpisodeService.retrieveOwnedEpisodesByMember(userDetails.getMember());
+        return new ResponseEntity<>(new ResponseMessage<>("소장 에피소드 조회 성공", response), HttpStatus.OK);
     }
 }
