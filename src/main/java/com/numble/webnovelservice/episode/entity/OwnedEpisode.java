02539134ec2be04c463bcca3dc1aba7dd2486fe8
suhjaesuk <suhjaesuk@gmail.com>
2023-04-13 11:13:33 +0900

    feat: OwnedEpisode 엔티티 클래스 추가 및 객체를 생성하는 팩토리 메서드 추가 / 열람 시  읽음 상태와 현재 읽는 페이지를 변경하는 메서드 추가, 페이지를 넘길 때 현재 페이지를 변경하는 메서드 추가

---
+++ src/main/java/com/numble/webnovelservice/episode/entity/OwnedEpisode.java
@@ -0,0 +1,80 @@
+package com.numble.webnovelservice.episode.entity;
+
+import com.numble.webnovelservice.member.entity.Member;
+import com.numble.webnovelservice.util.time.Timestamped;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.util.Optional;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OwnedEpisode extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="owned_episode_id")
+    private Long id;
+
+    @Column(nullable = true)
+    private Integer currentReadingPage;
+
+    @Column(nullable = false)
+    private Boolean isRead;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="episode_id")
+    private Episode episode;
+
+    @Builder
+    public OwnedEpisode(Long id, Integer currentReadingPage, Boolean isRead, Member member, Episode episode) {
+
+        this.id = id;
+        this.currentReadingPage = currentReadingPage;
+        this.isRead = isRead;
+        this.member = member;
+        this.episode = episode;
+    }
+
+    public static OwnedEpisode createOwnedEpisode(Member member, Episode episode) {
+
+        return OwnedEpisode.builder()
+                .isRead(false)
+                .currentReadingPage(null)
+                .episode(episode)
+                .member(member)
+                .build();
+    }
+
+    public void markAsRead(){
+
+        this.isRead = true;
+        this.currentReadingPage = Optional.ofNullable(currentReadingPage)
+                                    .orElse(1);
+    }
+
+    public void readNextPage(){
+
+        currentReadingPage++;
+    }
+
+    public void readPreviousPage(){
+
+        currentReadingPage--;
+    }
+}
