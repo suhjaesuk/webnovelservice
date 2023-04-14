cbd4a6071119455f05355bc4db1167da9d0f8caf
suhjaesuk <suhjaesuk@gmail.com>
2023-04-14 09:10:42 +0900

    feat: Novel로 Episode Optonal 리스트를 찾는 메서드 추가

--- src/main/java/com/numble/webnovelservice/episode/repository/EpisodeRepository.java
+++ src/main/java/com/numble/webnovelservice/episode/repository/EpisodeRepository.java
@@ -3,5 +3,10 @@ package com.numble.webnovelservice.episode.repository;
 import com.numble.webnovelservice.episode.entity.Episode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface EpisodeRepository extends JpaRepository<Episode, Long> {
+
+   Optional<List<Episode>> findByNovelId(Long novelId);
 }
