fb2e00a6a3d9f865f2d420c856081bf886e8b987
suhjaesuk <suhjaesuk@gmail.com>
2023-04-30 00:40:50 +0900

    fix: 조회수 증가는 원자적으로 실행되어야 한다.

--- src/main/java/com/numble/webnovelservice/episode/repository/EpisodeRepository.java
+++ src/main/java/com/numble/webnovelservice/episode/repository/EpisodeRepository.java
@@ -2,6 +2,7 @@ package com.numble.webnovelservice.episode.repository;
 
 import com.numble.webnovelservice.episode.entity.Episode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +13,8 @@ public interface EpisodeRepository extends JpaRepository<Episode, Long> {
 
    @Query("SELECT e FROM Episode e LEFT JOIN FETCH e.ownedEpisodes oe WHERE e.novel.id = :novelId")
    Optional<List<Episode>> findByNovelIdWithOwnedEpisodes(@Param("novelId") Long novelId);
+
+   @Modifying
+   @Query("UPDATE Episode e SET e.viewCount = e.viewCount + 1 WHERE e.id = :episodeId")
+   void increaseViewCountAtomically(@Param("episodeId") Long episodeId);
 }
--- src/main/java/com/numble/webnovelservice/novel/repository/NovelRepository.java
+++ src/main/java/com/numble/webnovelservice/novel/repository/NovelRepository.java
@@ -3,6 +3,9 @@ package com.numble.webnovelservice.novel.repository;
 import com.numble.webnovelservice.novel.entity.Genre;
 import com.numble.webnovelservice.novel.entity.Novel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,4 +18,8 @@ public interface NovelRepository extends JpaRepository<Novel, Long> {
     List<Novel> findByOrderByUpdatedAtDesc();
 
     List<Novel> findByIdIn(List<Long> novelId);
+
+    @Modifying
+    @Query("UPDATE Novel n SET n.totalViewCount = n.totalViewCount + 1 WHERE n.id = :novelId")
+    void increaseTotalViewCountAtomically(@Param("novelId") Long novelId);
 }
